05d2afd7c525bb8e644d942f77430d62449715bd
saulosjoseph <saulos.joseph@gmail.com>
2025-03-04 04:54:22 -0300

    feat: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -6,12 +7,12 @@ export default function Home() {
   return (
     <div className="relative w-full h-screen flex justify-center items-center bg-gray-100">
       {/* Imagem de fundo fixa no canto inferior direito */}
-      <div className="absolute bottom-0 right-0 w-1/5 h-1/5 bg-no-repeat bg-contain" style={{ backgroundImage: "url('/images/pet-background.webp')" }}></div>
+      <div className="z-30 absolute bottom-0 right-0 w-full h-full bg-no-repeat bg-contain" style={{ backgroundImage: "url('/images/pet-background.webp')" }}></div>
       
       {/* Botão iniciar */}
       <button
         onClick={() => router.push('/dashboard')}
-        className="px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 transition"
+        className="z-40 px-6 py-3 w-40 h-20 bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 transition"
       >
         Iniciar
       </button>
